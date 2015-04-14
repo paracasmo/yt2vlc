de4cd9b90d56b0712e8a5ddc165e8c22d2268d8e
paracasmo <paracasmo@users.noreply.github.com>
2015-04-14 19:42:40 +0200

    Parse fake html. Next up: stream http

--- yt2vlc.js
+++ yt2vlc.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+var htmlparser = require("htmlparser2");
+var parser = new htmlparser.Parser({
+	onopentag: function(name, attribs) {
+		if (name === "tr" && attribs.class.indexOf("pl-video") > -1) {
+			console.log(attribs["data-title"] + "(https://www.youtube.com/watch?v=" + attribs["data-video-id"] + ")");
+		}
+	}
+});
+parser.write("<tr class=\"pl-video yt-uix-tile \" data-title=\"Coffee with a Googler: Android Auto Product Manager Andrew Brenner\" data-video-id=\"QQF40qveBjg\" data-set-video-id=\"\">");
+parser.end();
