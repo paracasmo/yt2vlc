5b72f88ba7fc23710aa539cc12562df27b126045
paracasmo <paracasmo@users.noreply.github.com>
2015-04-23 19:53:46 +0200

    v0.1.0 - Does the trick, just don't look at it closely

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # yt2vlc
-Node module that allows transformation of YouTube playlist URLs to VLC playlist files.
+> Node module that allows transformation of YouTube playlist URLs to VLC playlist files.
+
+## CLI Usage
+
+```js
+yt2vlc <playlistId> <outputFile>
+```
+
+Definitely work in progress. Currently does its job.
--- yt2vlc.js
+++ yt2vlc.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var playlistId = process.argv[2];
 var outputFile = process.argv[3];
+var vidCount = 0;
 
 var video = function(id, title) {
 	this.id = id;
@@ -15,23 +16,45 @@ var fileStream = fs.createWriteStream(outputFile);
 var parser = new htmlparser.Parser({
 	onopentag: function(name, attribs) {
 		if (name === "tr" && attribs.class.indexOf("pl-video") > -1) {
-			var video = new video(attribs["data-video-id"], attribs["data-title"]);
-			fileStream.write(transform2xspf(video));
+			var curVideo = new video(attribs["data-video-id"], attribs["data-title"]);
+			fileStream.write(transform2xspf(curVideo));
+			vidCount++;
 		}
 	}
 });
 
 request('https://www.youtube.com/playlist?list=' + playlistId, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    parser.write(body);
-  }
-});
 
-function transform2xspf(video) {
-	return video.title + "(https://www.youtube.com/watch?v=" + video.id + ")\n";
-}
+	fileStream.write(writeHeader());
+	
+	if (!error && response.statusCode == 200) {
+		parser.write(body);
+	}
+	
+	fileStream.write(writeFooter());
+});
 
 fileStream.on('finish', function() {
 	parser.end();
 	fileStream.end();
 });
+
+function writeHeader() {
+	return '<?xml version="1.0" encoding="UTF-8"?>\n<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">\n<title>Playlist</title>\n<trackList>\n';
+}
+
+function transform2xspf(video) {
+	return '<track><location>https://www.youtube.com/watch?v=' + video.id + '</location><title>' + video.title + '</title><extension application="http://www.videolan.org/vlc/playlist/0"><vlc:id>' + vidCount + '</vlc:id><vlc:option>network-caching=1000</vlc:option></extension></track>\n';
+}
+
+function writeFooter() {
+	var top = '</trackList>\n<extension application="http://www.videolan.org/vlc/playlist/0">\n';
+	var middle = '';
+	for(i = 0; i<= vidCount; i++) {
+		middle+='<vlc:item tid="' + i + '"/>\n';
+	}
+	
+	var bottom = '</extension>\n</playlist>';
+
+	return top + middle + bottom;
+}
